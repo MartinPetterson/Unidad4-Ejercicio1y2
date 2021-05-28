6ab8f8254e241a10b5ea7e997d268cf600e1a755
Martin Petterson <martin.n.petterson@gmail.com>
2021-05-28 17:52:19 -0300

    agregue los archivos HTML al git

---
+++ Unidad4java.js
@@ -0,0 +1,7 @@
+var Pie, Bicicleta, Colectivo, Auto, Avion, distancia;
+distancia = prompt('ingrese la distancia','');
+
+if (distancia = (0,1000)){
+    document.write ('El sistema recomienda que camine');
+}if (distancia = (1000,10000))
+document.write ('El sistema recomienda que viaje en bicicleta')
